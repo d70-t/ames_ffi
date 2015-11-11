57e0dfee2d37809dac65be815efc19fa0574b6a4
Tobias Kölling <tobi@die70.de>
2015-11-11 20:29:02 +0100

    added repair functions for tabs and wrong_nlhead

--- ames/__init__.py
+++ ames/__init__.py
@@ -2,6 +2,53 @@
 ames helper tools
 """
 
+def isreal(string):
+    try:
+        float(string)
+    except ValueError:
+        return False
+    return True
+
+def maybe_value_line(line):
+    line_split = line.split()
+    return len(line_split) > 1 \
+           and all(isreal(x) for x in line_split)
+
+def repair_tabs(inp):
+    """
+    Replaces tabs with spaces
+    """
+    if '\t' in inp:
+        return inp.replace('\t', ' '), 1
+    else:
+        return inp, 0
+
+def repair_nlhead(inp):
+    """
+    Fixes wrong nlhead value
+    """
+    lines = inp.split('\n')
+    nlhead, rest = lines[0].split(None, 1)
+    nlhead = int(nlhead)
+
+    potential_nlhead = []
+    for i, (a, b) in enumerate(zip(lines, lines[1:]), 1):
+        if not maybe_value_line(a) \
+                and maybe_value_line(b):
+            potential_nlhead.append(i)
+    if nlhead != potential_nlhead[-1]:
+        # change is required
+        nlhead = potential_nlhead[-1]
+        lines[0] = '%d %s'%(nlhead, rest)
+        return '\n'.join(lines), 10
+    else:
+        return inp, 0
+
+REPAIR_FUNCTIONS = [
+    ('tabs', repair_tabs),
+    ('wrong_nlhead', repair_nlhead),
+    ]
+
 class AmesRepairTool(object):
     """
     Repair tool for broken NASA AMES Files
@@ -10,7 +57,13 @@ class AmesRepairTool(object):
         """
         Loads a broken input file and repairs it.
         """
-        return RepairResult(input_file.read(), [])
+        inp = input_file.read()
+        repaired = []
+        for repair_name, repair_function in REPAIR_FUNCTIONS:
+            inp, score = repair_function(inp)
+            if score > 0:
+                repaired.append(repair_name)
+        return RepairResult(inp, repaired)
 
 
 class RepairResult(object):
